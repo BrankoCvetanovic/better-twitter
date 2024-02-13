b012e7f6befca3314fbdb3070d08c00aa75fe1be
BahkoHatake <cbranko01@gmail.com>
2024-02-13 22:59:57 +0100

    Add retweet function

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,11 +1,15 @@
 import { FC, useEffect, useState } from "react";
 import { useRouteLoaderData } from "react-router-dom";
 import { NavLink } from "react-router-dom";
-import { getUserId } from "../util/auth";
-import { addLike, removeLike } from "../util/post";
+import { getUserId, getUserName } from "../util/auth";
+import { addLike, removeLike, uploadPost } from "../util/post";
 import { IconButton } from "@mui/material";
 import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
 import FavoriteIcon from "@mui/icons-material/Favorite";
+import IosShareIcon from "@mui/icons-material/IosShare";
+import { ToastContainer, toast } from "react-toastify";
+import { useAppDispatch } from "../store/hooks";
+import { newPostSliceActions } from "../store";
 
 const Post: FC<{
   text: string;
@@ -18,12 +22,12 @@ const Post: FC<{
   };
 }> = ({ text, imageName, userName, authId, postId, likes }) => {
   const [imageData, setImageData] = useState<string[]>([]);
-
   const [likesCount, setLikeCount] = useState(Object.keys(likes).length - 1);
-
   const [isLiked, setIsLiked] = useState(false);
 
   const loaderData = useRouteLoaderData("root") as string[];
+
+  const dispach = useAppDispatch();
 
   useEffect(() => {
     setImageData(loaderData);
@@ -42,6 +46,7 @@ const Post: FC<{
   }
 
   const currentUserId = getUserId();
+  const currentUsername = getUserName();
 
   function handleLike() {
     addLike(postId, currentUserId!);
@@ -55,13 +60,43 @@ const Post: FC<{
     setIsLiked(false);
   }
 
+  async function handleRetweet() {
+    const retweetText = `#${userName}: ${text}`;
+
+    const toastId = toast.loading("Please wait...");
+    const isTextError = await uploadPost(
+      currentUserId!,
+      retweetText,
+      imageName,
+      currentUsername!
+    );
+    if (isTextError) {
+      toast.update(toastId, {
+        render: "An error ocured",
+        type: "error",
+        isLoading: false,
+        autoClose: 4000,
+      });
+      return;
+    }
+    toast.update(toastId, {
+      render: "Succsess",
+      type: "success",
+      isLoading: false,
+      autoClose: 4000,
+    });
+    setTimeout(() => {
+      dispach(newPostSliceActions.updatePostState());
+    }, 1000);
+  }
+
   return (
     <div className="post">
+      <ToastContainer position="top-center" hideProgressBar />
       <NavLink className={"title"} to={`/profiles/${authId}`}>
         {userName}
       </NavLink>
       <div className="text"> {text}</div>
-      {isError && <p>An error occurred while fetching images.</p>}
       {imageName && <img src={imageData[imageIndex]} alt="" />}
       <div className="actions">
         <div className="like">
@@ -74,6 +109,12 @@ const Post: FC<{
           </IconButton>
           <div>{likesCount}</div>
         </div>
+        <IconButton
+          disabled={userName === currentUsername!}
+          onClick={handleRetweet}
+        >
+          <IosShareIcon />
+        </IconButton>
       </div>
     </div>
   );
